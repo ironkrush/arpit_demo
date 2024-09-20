dfed4929f79294a8aeac38262e774de5b5c0d513
ironkrush <krush2752@gmail.com>
2024-09-20 16:33:33 +0530

    landing div added and entry animation added

---
+++ lenis.js
@@ -0,0 +1,20 @@
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+gsap.set(".canv", { scale: 0.5 });
+
+gsap.to(".canv", {
+    scale: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".landing",
+        start: "bottom 99%",
+        scrub: 2,
+    }
+});
